62490385af8b910145ddca38f6abeba3af4b1303
hainet50b <20400616+hainet50b@users.noreply.github.com>
2023-07-03 17:41:06 +0900

    ShardingsphereをSpring Bootで取り扱うサンプルを追加した。

---
+++ src/test/java/com/programacho/shardingspherespringbootgym/ShardingsphereSpringBootGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programacho.shardingspherespringbootgym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingsphereSpringBootGymApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
